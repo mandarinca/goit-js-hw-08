33173a5d937c46e19a11c1bbc82c502b136657cf
mandarinca <myler4ik@gmail.com>
2023-04-29 20:08:32 +0300

    01-gallery -  done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+
+const ulEl = document.querySelector('.gallery');
+
+galleryItems.forEach(el => {
+ulEl.insertAdjacentHTML(
+    'beforeEnd',
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+        <img
+            class="gallery__image"
+            src="${el.preview}"
+            alt="${el.description}"
+            
+        />
+        </a>
+    </li>`
+);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+captionsData: 'alt',
+captionDelay: 250,
+});
+
