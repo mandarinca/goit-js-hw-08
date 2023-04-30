588ec5645fb961e1da58ee875d0dae8071d5e2ed
mandarinca <myler4ik@gmail.com>
2023-04-30 21:32:41 +0300

    Task #2 of HW-08 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+
+player.on('play', function () { 
+
+console.log('playEvent: played the video!');
+});
+
+function upDate(data) {
+    console.log(data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(upDate, 1000));
+
+console.log(localStorage.getItem('videoplayer-current-time'));
+
+let lastVideoTime = localStorage.getItem('videoplayer-current-time');
+
+    player.setCurrentTime(lastVideoTime)
+        .then(function (seconds) {
+        console.log("Ура :", seconds);
+        }).catch(function (error) {
+            switch (error.name) {
+                case 'RangeError':
+                    console.log("Out of range!!!");
+                    break;
+                default:
+                    console.log('Unkown error=(');
+                    break;
+        }
+    });
+
+    player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+    });
